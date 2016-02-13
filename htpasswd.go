6e0db3394f4d2dab8947fdfe0f97b3ac9ad5743d
Jan Halfar <jan@bestbytes.com>
2016-02-13 19:01:07 +0100

    added tests

--- htpasswd.go
+++ htpasswd.go
@@ -1,7 +1,10 @@
+// Package htpasswd is utility package to manipulate htpasswd files. I supports\
+// bcrypt and sha hashes.
 package htpasswd
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"strings"
 )
@@ -15,8 +18,8 @@ type HashAlgorithm string
 const (
 	// HashBCrypt bcrypt - recommended
 	HashBCrypt = "bcrypt"
-	// HashSHA5 sha5 insecure - do not use
-	HashSHA5 = "sha5"
+	// HashSHA sha5 insecure - do not use
+	HashSHA = "sha"
 )
 
 const (
@@ -45,13 +48,16 @@ func (hp HashedPasswords) WriteToFile(file string) error {
 
 // SetPassword set a password for a user with a hashing algo
 func (hp HashedPasswords) SetPassword(name, password string, hashAlgorithm HashAlgorithm) (err error) {
+	if len(password) == 0 {
+		return errors.New("passwords must not be empty, if you want to delete a user call RemoveUser")
+	}
 	hash := ""
 	prefix := ""
 	switch hashAlgorithm {
 	case HashBCrypt:
 		prefix = "$2y$"
 		hash, err = hashBcrypt(password)
-	case HashSHA5:
+	case HashSHA:
 		prefix = "{SHA}"
 		hash = hashSha(password)
 	}
@@ -77,19 +83,29 @@ func ParseHtpasswdFile(file string) (passwords HashedPasswords, err error) {
 
 // ParseHtpasswd parse htpasswd bytes
 func ParseHtpasswd(htpasswdBytes []byte) (passwords HashedPasswords, err error) {
-	lines := strings.Split(LineSeparator, string(htpasswdBytes))
+	lines := strings.Split(string(htpasswdBytes), LineSeparator)
 	passwords = make(map[string]string)
-	for _, line := range lines {
+	for lineNumber, line := range lines {
 		// scan lines
 		line = strings.Trim(line, " ")
 		if len(line) == 0 {
 			// skipping empty lines
 			continue
 		}
-		parts := strings.Split(PasswordSeparator, line)
+		parts := strings.Split(line, PasswordSeparator)
+		if len(parts) != 2 {
+			err = errors.New(fmt.Sprintln("invalid line", lineNumber+1, "unexpected number of parts split by", PasswordSeparator, len(parts), "instead of 2 in\"", line, "\""))
+			return
+		}
 		for i, part := range parts {
 			parts[i] = strings.Trim(part, " ")
 		}
+		_, alreadyExists := passwords[parts[0]]
+		if alreadyExists {
+			err = errors.New("invalid htpasswords file - user " + parts[0] + " was already defined")
+			return
+		}
+		passwords[parts[0]] = parts[1]
 	}
 	return
 }
@@ -101,6 +117,34 @@ func SetHtpasswdHash(file, name, hash string) error {
 		return err
 	}
 	passwords[name] = hash
+	return passwords.WriteToFile(file)
+}
+
+// RemoveUser remove an existing user from a file, returns an error, if the user does not \
+// exist in the file
+func RemoveUser(file, user string) error {
+	passwords, err := ParseHtpasswdFile(file)
+	if err != nil {
+		return err
+	}
+	_, ok := passwords[user]
+	if !ok {
+		return errors.New("user did not exist in file")
+	}
+	delete(passwords, user)
+	return passwords.WriteToFile(file)
+}
+
+// SetPasswordHash directly set a hash for a user in a file
+func SetPasswordHash(file, user, hash string) error {
+	if len(hash) == 0 {
+		return errors.New("you might want to rethink your hashing algorithm, it left you with an empty hash")
+	}
+	passwords, err := ParseHtpasswdFile(file)
+	if err != nil {
+		return err
+	}
+	passwords[user] = hash
 	return passwords.WriteToFile(file)
 }
 
--- htpassword_test.go
+++ htpassword_test.go
@@ -1,17 +1,106 @@
 package htpasswd
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+func poe(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func getHashedPasswords() HashedPasswords {
 	return HashedPasswords(map[string]string{
 		HashBCrypt: "$2y$05$ClsF7kLEDvRXnKnqgYvKnOTq5lLoQ.etyJacxsHO2gGZezPKO/Lua",
-		HashSHA5:   "{SHA}2PRZAyDhNDqRW2OUFwZQqPNdaSY=",
-		//test-md5:$apr1$M0Vyedud$k3ny5o2VBoWKsIDRcI53W0
+		HashSHA:    "{SHA}2PRZAyDhNDqRW2OUFwZQqPNdaSY=",
 	})
+}
+
+func tFile(name string) string {
+	f, err := ioutil.TempFile(os.TempDir(), "foomo-htpasswd-test-"+name)
+	poe(err)
+	return f.Name()
+}
+
+func fileContentsAre(t *testing.T, file string, contents string) {
+	fileBytes, err := ioutil.ReadFile(file)
+	poe(err)
+	if contents != string(fileBytes) {
+		t.Fatal("unexpected file contents", "should have been", contents, "was \""+string(fileBytes)+"\"")
+	}
+}
+
+func TestParseHtpassd(t *testing.T) {
+	passwords, err := ParseHtpasswd([]byte("sha:{SHA}2PRZAyDhNDqRW2OUFwZQqPNdaSY=\n"))
+	poe(err)
+	if len(passwords) != 1 {
+		t.Fatal("unexpected length in passwords")
+	}
+	const expected = "{SHA}2PRZAyDhNDqRW2OUFwZQqPNdaSY="
+	if passwords["sha"] != expected {
+		t.Fatal("sha password was wrong", passwords["sha"], "but expected", expected)
+	}
+}
+
+func TestEmptyHtpasswdFile(t *testing.T) {
+	f := tFile("empty")
+	SetPassword(f, "sha", "sha", HashSHA)
+	fileContentsAre(t, f, "sha:{SHA}2PRZAyDhNDqRW2OUFwZQqPNdaSY=\n")
+}
+
+func TestRemoveUser(t *testing.T) {
+	f := tFile("removeUser")
+	const firstUser = "sha"
+	SetPassword(f, firstUser, "sha", HashSHA)
+	const user = "foo"
+	SetPassword(f, user, "bar", HashBCrypt)
+	RemoveUser(f, user)
+	fileContentsAre(t, f, "sha:{SHA}2PRZAyDhNDqRW2OUFwZQqPNdaSY=\n")
+	passwordsFromFile, err := ParseHtpasswdFile(f)
+	poe(err)
+	if passwordsFromFile[firstUser] != "{SHA}2PRZAyDhNDqRW2OUFwZQqPNdaSY=" {
+		t.Fatal("failed to read right data from manipulated file")
+	}
+}
+
+func TestCorruption(t *testing.T) {
+	hasToE := func(err error, topic string) {
+		if err == nil {
+			t.Fatal("missed to get an error for", topic)
+		}
+	}
+	const (
+		corruptLine       = "foo:bar:bu\n"
+		corruptRepeatUser = "foo:pwd\nbar:bla\nfoo:fooagain\n"
+	)
+	_, eLine := ParseHtpasswd([]byte(corruptLine))
+	_, eUser := ParseHtpasswd([]byte(corruptRepeatUser))
+	hasToE(eLine, "corrupt line")
+	hasToE(eUser, "corrupt user repetition")
+}
+
+func TestSetPasswordHash(t *testing.T) {
+	f := tFile("set-hashes")
+	poe(SetPasswordHash(f, "a", "a"))
+	poe(SetPasswordHash(f, "b", "b"))
+	poe(SetPasswordHash(f, "c", "c"))
+	poe(RemoveUser(f, "b"))
+	passwords, err := ParseHtpasswdFile(f)
+	poe(err)
+	if passwords["a"] != "a" {
+		t.Fatal("a failed")
+	}
+	if passwords["b"] != "" {
+		t.Fatal("b failed")
+	}
+	if passwords["c"] != "c" {
+		t.Fatal("c failed")
+	}
 }
 
 func TestHashing(t *testing.T) {
@@ -23,9 +112,9 @@ func TestHashing(t *testing.T) {
 			t.Fatal(err)
 		}
 		switch algo {
-		case HashSHA5:
+		case HashSHA:
 			if hash != testHashes[name] {
-				t.Fatal("sha fuck up", hash, testHashes[name])
+				t.Fatal(algo, hash, testHashes[name])
 			}
 		case HashBCrypt:
 			err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(name))
